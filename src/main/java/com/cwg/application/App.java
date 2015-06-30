688acbc60beff85946544046a826e0c4e4dc9124
dusty <chidumekwueme@gmail.com>
2015-06-30 12:49:10 +0100

    i just added this change

--- src/main/java/com/cwg/application/App.java
+++ src/main/java/com/cwg/application/App.java
@@ -7,4 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class App {
+
+    public static void main(String[] args) {
+        //this is a test method i just wrote
+    }
 }
