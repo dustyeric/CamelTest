954d74d637460b4871997b46c99aa052e74dc5b4
dusty <dusty@dustyhost.org>
2015-06-30 12:43:42 +0100

    Initial commit

---
+++ src/main/java/com/cwg/application/App.java
@@ -0,0 +1,10 @@
+package com.cwg.application;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by dusty on 6/30/15.
+ */
+@SpringBootApplication
+public class App {
+}
